9783a945aeb3a5b7e176ce88abcc19f668a44450
Ameerah <muhammadameerahashir11@gmail.com>
2023-11-23 12:29:45 +0100

    ghrt

--- components/landingpage/Testimonial.jsx
+++ components/landingpage/Testimonial.jsx
@@ -98,6 +98,8 @@ const Testimonial = () => {
                 <p className="text-black dark:text-white text-base">
                   "Uniqueness at its peak"
                 </p>
+              </div>
+            </div>
           {/* circular */}
           {/* <div className="w-fit h-auto p-4 hidden md:flex justify-center items-center rounded-full shadow-md bg-white">
             <IoIosArrowForward className="text-blue text-2xl" />
